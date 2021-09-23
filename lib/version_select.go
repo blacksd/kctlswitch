5cd56f26d2797bd23ce18dfb8ccae5ec27c4a664
Marco Bulgarini <marco.bulgarini@gmail.com>
2021-09-23 10:39:49 +0200

    temporary dir handling

--- lib/version_select.go
+++ lib/version_select.go
@@ -2,6 +2,10 @@ package lib
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"sort"
 
 	"github.com/Masterminds/semver/v3"
@@ -28,6 +32,10 @@ func KctlVersionList(constraint string, log *zap.SugaredLogger) ([]string, error
 
 	// TODO: use a local cache for git
 	// TODO: manage cache expiration
+
+	// asd, _ := getTemporaryGitDir(log)
+	// log.Info(asd)
+
 	rem := git.NewRemote(memory.NewStorage(), &config.RemoteConfig{
 		Name: "origin",
 		URLs: []string{"https://github.com/kubernetes/kubernetes.git"},
@@ -72,4 +80,25 @@ func validateTag(tag string, constraint semver.Constraints) error {
 		return ErrVersionNotSelected
 	}
 	return nil
+}
+
+func getTemporaryGitDir(log *zap.SugaredLogger) (path string, err error) {
+	parentDir := os.TempDir()
+	globPattern := filepath.Join(parentDir, "kctlswitch-*")
+	matches, _ := filepath.Glob(globPattern)
+	var tempGitDir string
+	if len(matches) == 0 {
+		log.Debug("Didn't find a match for a temp dir, building one.")
+		tempGitDir, err = ioutil.TempDir(parentDir, "kctlswitch-*")
+		if err != nil {
+			return "", err
+		}
+
+	} else {
+		log.Debugf("Found a matching temp dir \"%s\", using that", matches[0])
+		tempGitDir = matches[0]
+	}
+	tempGitPath := fmt.Sprintf("%s/.git", tempGitDir)
+	os.MkdirAll(tempGitPath, 0755)
+	return tempGitPath, nil
 }
